411e935aed262dc08cece204135d7ddcdfaa2a71
Arnaud Lafon <arnaud.lafon@proton.me>
2023-04-30 18:41:59 +0200

    feat: animate search block

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -25,9 +25,9 @@ const Search = () => {
     <Layout>
       <div className="section">
         <div className="container">
-          <motion.h1 className="is-size-2 has-text-link has-text-centered has-text-weight-bold">
+          <h1 className="is-size-2 has-text-link has-text-centered has-text-weight-bold">
             Une playlist pour toi !
-          </motion.h1>
+          </h1>
           <p className="has-text-white is-size-7 has-text-centered has-text-weight-semibold">
             Balance jusqu&apos;à 5 de tes musiques ou artistes préférés
           </p>
@@ -41,7 +41,12 @@ const Search = () => {
 
       {
         tracks.length >= 1 &&
-        <div className="section has-background-white">
+        <motion.div
+          className="section has-background-white"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: .2 }}
+        >
           <div className="container">
             <h2 className="is-size-3 has-text-black has-text-weight-bold mb-3">Tes coups de coeur</h2>
             <div className={`columns is-mobile is-multiline ${tracks.length > 3 ? 'is-justify-content-center' : ''}`}>
@@ -51,7 +56,7 @@ const Search = () => {
                     <motion.div
                       initial={{ y: 50, filter: 'blur(10px)' }}
                       animate={{ y: 0, filter: 'blur(0px)' }}
-                      transition={{ ease: "easeOut", duration: .5, type: 'sprint' }}
+                      transition={{ ease: "easeOut", duration: 1, type: 'spring' }}
                     >
                       <Card
                         title={track.name}
@@ -77,7 +82,7 @@ const Search = () => {
             </div>
 
           </div>
-        </div>
+        </motion.div>
       }
     </Layout>
   )
