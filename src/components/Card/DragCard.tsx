992f6d56f5c0beaca6febfd492b668af39f26aca
Arnaud Lafon <arnaud.lafon@proton.me>
2023-05-01 11:31:42 +0200

    feat: add swipe gesture

--- src/components/Card/DragCard.tsx
+++ src/components/Card/DragCard.tsx
@@ -1,18 +1,19 @@
 import { Swipe } from "@/enums";
+import { Track } from "@/models/track";
 import { motion, PanInfo } from "framer-motion";
 import { FC, useState } from "react";
-import tracks from '../../../mockedData';
 
 interface IDragCard {
   isActive: boolean;
+  track: Track;
   onSwipe: Function;
 }
 
-const DragCard: FC<IDragCard> = ({isActive, onSwipe}) => {
+const DragCard: FC<IDragCard> = ({ isActive, onSwipe, track }) => {
   const onDrag = (_event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
-    if (info.offset.x > 100) {
+    if (info.offset.x > 50) {
       setSwipe(Swipe.Like)
-    } else if (info.offset.x < -100) {
+    } else if (info.offset.x < -50) {
       setSwipe(Swipe.Dislike)
     } else {
       setSwipe(undefined)
@@ -20,61 +21,73 @@ const DragCard: FC<IDragCard> = ({isActive, onSwipe}) => {
   }
 
   const onDragEnd = () => {
-    return onSwipe(swipe, tracks[0])
+    console.log('SSS', swipe);
+    return onSwipe(swipe, track)
   }
 
   const [swipe, setSwipe] = useState<Swipe>()
 
   return (
-    <motion.div
-      key={tracks[0].id}
-      className="card"
-      drag={true}
-      whileDrag={
-        swipe === Swipe.Like
-          ? { scale: 0.9, opacity: .6, rotate: '6deg' }
-          : swipe === Swipe.Dislike
-            ? { scale: 0.9, opacity: .6, rotate: '-6deg' }
-            : undefined
-      }
-      dragConstraints={{ top: 0, bottom: 0 }}
-      dragElastic={.3}
-      // dragSnapToOrigin={true}
-      onDrag={onDrag}
-      onDragEnd={onDragEnd}
-      initial={{
-        scale: .9
-      }}
-      animate={{
-        scale: 1,
-        // rotate: `${card.name.length % 2 === 0 ? 6 : -6}deg`,
-      }}
-      exit={{ opacity: 0 }}
-    >
-      <div className="card-image">
-        <figure className="image is-square">
-          <img style={{ pointerEvents: 'none' }} src={tracks[0].album.images[0].url} alt="Placeholder image" />
-        </figure>
-      </div>
-      {/* <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+    isActive
+      ? <motion.div
+        key={track.id}
+        className="card draggable-card"
+        drag={true}
+        whileDrag={
+          swipe === Swipe.Like
+            ? { scale: 0.9, opacity: .6, rotate: '6deg' }
+            : swipe === Swipe.Dislike
+              ? { scale: 0.9, opacity: .6, rotate: '-6deg' }
+              : undefined
+        }
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={.3}
+        onDrag={onDrag}
+        onDragEnd={onDragEnd}
+        initial={{
+          scale: .9
+        }}
+        animate={{
+          scale: 1,
+          // rotate: `${card.name.length % 2 === 0 ? 6 : -6}deg`,
+        }}
+        exit={
+          swipe === Swipe.Like
+            ? { scale: 0.85, opacity: 0, rotate: '6deg' }
+            : swipe === Swipe.Dislike
+              ? { scale: 0.85, opacity: 0, rotate: '-6deg' }
+              : undefined
+        }
+      >
+        <div className="card-image">
+          <figure className="image is-square">
+            <img style={{ pointerEvents: 'none' }} src={track.album.images[0].url} alt="Placeholder image" />
           </figure>
         </div>
-        <div className="media-content">
-          <p className="title is-4">{track.name}</p>
-          <p className="subtitle is-6">@${track.artists[0].name}</p>
-        </div>
-      </div>
-    </div> */}
-      <footer className="card-footer">
-        <a href="#" className="card-footer-item">Save</a>
-        <a href="#" className="card-footer-item">Edit</a>
-        <a href="#" className="card-footer-item">Delete</a>
-      </footer>
-    </motion.div>
+        <footer className="card-footer has-background-white">
+          <a href="#" className="card-footer-item">Save</a>
+          <a href="#" className="card-footer-item">Edit</a>
+          <a href="#" className="card-footer-item">Delete</a>
+        </footer>
+      </motion.div>
+      : (
+        <motion.div
+          initial={{
+            scale: .9
+          }}
+          className="card draggable-card">
+          <div className="card-image">
+            <figure className="image is-square">
+              <img style={{ pointerEvents: 'none' }} src={track.album.images[0].url} alt="Placeholder image" />
+            </figure>
+          </div>
+          <footer className="card-footer has-background-white">
+            <a href="#" className="card-footer-item">Save</a>
+            <a href="#" className="card-footer-item">Edit</a>
+            <a href="#" className="card-footer-item">Delete</a>
+          </footer>
+        </motion.div>
+      )
   )
 }
 
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,27 +1,41 @@
 import DragCard from '@/components/Card/DragCard';
 import Layout from '@/components/Layout';
 import { Swipe } from '@/enums';
+import { Track } from '@/models/track';
+import { AnimatePresence } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
+import mockedData from '../../mockedData';
 
 const Play = () => {
-  const [playlist, setPlaylist] = useState([]);
+  const [tracks, setTracks] = useState<Track[]>(mockedData as any);
+  const [playlist, setPlaylist] = useState<Track[]>([]);
+  const activeIndex = tracks.length - 1;
 
-  const onSwipe = (swipe: Swipe, track: any) => {
-    console.log(swipe, track);
+  console.log('activeIndex', activeIndex);
+  
+
+  const onSwipe = (swipe: Swipe, track: Track) => {
+    console.log('onSwipe', swipe);
+    if (swipe === undefined) return;
+    setTracks((current) => current.filter((t) => t.id !== track.id))
     if (swipe === Swipe.Like) {
-      return onLike();
+      return onLike(track);
     }
     if (swipe === Swipe.Dislike) {
-      return onDislike();
+      return onDislike(track);
     }
     return;
   }
 
-  const onLike = () => {
+  const onLike = (track: Track) => {
     console.log('like');
+    setPlaylist([
+      ...playlist,
+      track,
+    ])
   }
 
-  const onDislike = () => {
+  const onDislike = (track: Track) => {
     console.log('dislike');
   }
 
@@ -31,13 +45,36 @@ const Play = () => {
         <div className='container'>
           <div className='columns is-centered'>
             <div className='column is-half is-relative'>
+              <AnimatePresence >
+                {
+                  tracks.map((track: any, index: number) => {
+                    // tracks.slice().reverse().map((track: any, index: number) => {
+                    // console.log(track.name, index);
+                    return (
+                      <DragCard
+                        key={track.id}
+                        isActive={index === activeIndex}
+                        track={track}
+                        onSwipe={onSwipe}
+                      />
+                    )
+                  }
+                  )
+                }
+              </AnimatePresence>
 
-              <DragCard
-                isActive={true}
-                onSwipe={onSwipe}
-              />
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className='section'>
+        <div className='container'>
+          {
+            playlist.map((p) => (
+              <div key={p.id}>{p.name}</div>
+            ))
+          }
         </div>
       </div>
     </Layout>
