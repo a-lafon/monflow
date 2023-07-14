47afcd24d5a41245b217e3c40c3fb7f1d1b6324a
Arnaud Lafon <arnaud.lafon@proton.me>
2023-07-14 12:43:42 +0200

    feat: add refresh button when track list is empty

--- src/presentation/components/Swipe/DragCardContainer/DragCardContainer.tsx
+++ src/presentation/components/Swipe/DragCardContainer/DragCardContainer.tsx
@@ -1,8 +1,9 @@
 import { Track } from "@/domain/models/track";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { FC, useEffect, useState } from "react";
 import DragCard from "./DragCard";
 import { SwipeType } from "@/domain/enums";
+import { FiRefreshCcw } from "react-icons/fi";
 
 interface IDragCardContainer {
   tracks: Track[];
@@ -14,6 +15,8 @@ interface IDragCardContainer {
 
 const DragCardContainer: FC<IDragCardContainer> = ({ onLike, onDislike, onTrackChange, onRefresh, ...props }) => {
   const [tracks, setTracks] = useState<Track[]>([]);
+  const [dislikedTrackIds, setDisslikedTrackIds] = useState<string[]>([]);
+  const [lastTrackLiked, setLastTrackLiked] = useState<Track>();
   const activeIndex = tracks.length - 1;
 
   useEffect(() => {
@@ -25,7 +28,6 @@ const DragCardContainer: FC<IDragCardContainer> = ({ onLike, onDislike, onTrackC
       return;
     }
     const currentTrak = tracks[activeIndex];
-    console.log('currentTrak', currentTrak);
     onTrackChange(currentTrak);
   }, [activeIndex])
 
@@ -38,12 +40,36 @@ const DragCardContainer: FC<IDragCardContainer> = ({ onLike, onDislike, onTrackC
     setTracks((current) => current.filter((t) => t.id !== track.id))
 
     if (swipe === SwipeType.Like) {
+      setLastTrackLiked(track);
       return onLike(track);
     }
 
     if (swipe === SwipeType.Dislike) {
+      setDisslikedTrackIds([...dislikedTrackIds, track.id]);
       return onDislike(track);
     }
+  }
+
+  if (tracks.length === 0) {
+    const tracksWithoutDisslikes = props.tracks.filter((track) => dislikedTrackIds.includes(track.id));
+    const randomIndex = Math.floor(Math.random() * tracksWithoutDisslikes.length);
+    const track = lastTrackLiked || tracksWithoutDisslikes[randomIndex];
+    return (
+      <div className="mt-6 has-text-centered">
+        <motion.button
+          className="button is-large is-rounded is-link"
+          initial={{scale: 0.9}}
+          animate={{ scale: 1.2 }}
+          transition={{ type: "spring", duration: .8 }}
+          onClick={() => onRefresh(track)}
+        >
+          <span className="icon is-large">
+            <FiRefreshCcw />
+          </span>
+          <span>Continuer</span>
+        </motion.button>
+      </div>
+    )
   }
 
   return (
